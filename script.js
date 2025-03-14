a93041b497ebbbbba4355aa88a2531b4531de177
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-03-15 01:13:50 +0700

    feat: add horizontal win check

--- script.js
+++ script.js
@@ -32,20 +32,59 @@ const GameController = (function() {
 
   const switchActivePlayer = () => (activePlayer === player1) ? activePlayer = player2 : activePlayer = player1;
 
+  const checkWin = () => {
+    const board = GameBoard.getBoard();
+    
+    // Horizontal check
+    for (let i = 0; i < board.length; i++) {
+      const first = board[i][0];
+      if (first === null) continue;
+      let isWin = true;
+      for (let j = 1; j < board[i].length; j++) {
+        if (board[i][j] !== first) {
+          isWin = false;
+          break;
+        }
+      }
+      if (isWin) return true;
+    }
+
+    // Vertical
+    for (let i = 0; i < board.length; i++) {
+      const first = board[0][0];
+    }
+
+    return false;
+  };
+
   const playRound = () => {
     console.log(GameBoard.getBoard());
-    GameBoard.markBoard(getActivePlayer().marker, 0, 2);
+    GameBoard.markBoard(getActivePlayer().marker, 0, 0);
     switchActivePlayer();
     
+    console.log(GameBoard.getBoard());
+    GameBoard.markBoard(getActivePlayer().marker, 1, 0);
+    switchActivePlayer();
+
     console.log(GameBoard.getBoard());
     GameBoard.markBoard(getActivePlayer().marker, 0, 1);
     switchActivePlayer();
 
     console.log(GameBoard.getBoard());
-    GameBoard.markBoard(getActivePlayer().marker, 2, 0);
+    GameBoard.markBoard(getActivePlayer().marker, 1, 1);
     switchActivePlayer();
 
     console.log(GameBoard.getBoard());
+    GameBoard.markBoard(getActivePlayer().marker, 0, 2);
+
+    console.log(GameBoard.getBoard());
+    
+    const isWin = checkWin();
+    if (isWin) console.log(`${getActivePlayer().name} win!`);
+
+    switchActivePlayer();
+
+    
   };
 
   return { getActivePlayer, playRound };
